dbd233c7f2c7fc1a6dccd03fa94e812963a0535d
Sammm <ntdsam@gmail.com>
2024-08-03 22:41:32 +0700

    complete setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="register">
+      <div className="register_content">
+        <form></form>
+        <a>Already have an account? Login here</a>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
