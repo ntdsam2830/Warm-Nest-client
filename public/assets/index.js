4c8d5fafe7a776643d5d055de55c2d972a28581f
Sammm <ntdsam@gmail.com>
2024-08-04 16:43:47 +0700

    create login page

---
+++ public/assets/index.js
@@ -0,0 +1,7 @@
+import addImage from "./addImage.png";
+import arctic_cat from "./arctic_cat.webp";
+import barn_cat from "./barn_cat.jpg";
+import login from "./login.png";
+import register from "./register.jpg";
+
+export { addImage, arctic_cat, barn_cat, login, register };
